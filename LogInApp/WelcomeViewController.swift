e34d5d6c70e162c3414c12e1e04afed5ec26d29f
Roman <roman.d.kozlov@gmail.com>
2021-03-31 00:59:53 +0300

    storyboard done

---
+++ LogInApp/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  LogInApp
+//
+//  Created by Roman Kozlov on 31.03.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
